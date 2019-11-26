6296374552037601c801bb12d4031e313ddc9c92
wailovet <19940108>
2019-11-26 12:05:29 +0800

    `update`

--- samples/ouxuanac/ouxuanac_sample.c
+++ samples/ouxuanac/ouxuanac_sample.c
@@ -10,51 +10,60 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/ioctl.h>
-#include <net/if.h>        //for struct ifreq
+#include <net/if.h> //for struct ifreq
 
-int get_mac(char * mac, int len_limit)    //返回值是实际写入char * mac的字符个数（不包括'\0'）
+int get_mac(char *mac, int len_limit) //返回值是实际写入char * mac的字符个数（不包括'\0'）
 {
     struct ifreq ifreq;
     int sock;
 
-    if ((sock = socket (AF_INET, SOCK_STREAM, 0)) < 0)
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
-        perror ("socket");
+        perror("socket");
         return -1;
     }
-    strcpy (ifreq.ifr_name, "eth0");    //Currently, only get eth0
+    strcpy(ifreq.ifr_name, "eth0"); //Currently, only get eth0
 
-    if (ioctl (sock, SIOCGIFHWADDR, &ifreq) < 0)
+    if (ioctl(sock, SIOCGIFHWADDR, &ifreq) < 0)
     {
-        perror ("ioctl");
+        perror("ioctl");
         return -1;
     }
-    
-    return snprintf (mac, len_limit, "%X:%X:%X:%X:%X:%X", (unsigned char) ifreq.ifr_hwaddr.sa_data[0], (unsigned char) ifreq.ifr_hwaddr.sa_data[1], (unsigned char) ifreq.ifr_hwaddr.sa_data[2], (unsigned char) ifreq.ifr_hwaddr.sa_data[3], (unsigned char) ifreq.ifr_hwaddr.sa_data[4], (unsigned char) ifreq.ifr_hwaddr.sa_data[5]);
+
+    return snprintf(mac, len_limit, "%X-%X-%X-%X-%X-%X", (unsigned char)ifreq.ifr_hwaddr.sa_data[0], (unsigned char)ifreq.ifr_hwaddr.sa_data[1], (unsigned char)ifreq.ifr_hwaddr.sa_data[2], (unsigned char)ifreq.ifr_hwaddr.sa_data[3], (unsigned char)ifreq.ifr_hwaddr.sa_data[4], (unsigned char)ifreq.ifr_hwaddr.sa_data[5]);
 }
 
-int main()
+int get_device_secret(DeviceInfo *pDevInfo)
 {
- 
+    return IOT_DynReg_Device(pDevInfo);
+}
+
+int main(int argc, char **argv)
+{
+    int code;
     //init log level
     IOT_Log_Set_Level(eLOG_DEBUG);
+    if (argc < 2)
+    {
+        printf("[error]  txc_ouxuanac [get_device_secret]");
+        return 0;
+    }
 
-    int ret;
     DeviceInfo sDevInfo;
-    bool infoNullFlag = false;
 
     memset((char *)&sDevInfo, 0, sizeof(DeviceInfo));
 
-    ret = HAL_GetDevInfo(&sDevInfo);
- 
-    int nRtn = get_mac(sDevInfo.device_name, sizeof(sDevInfo.device_name));
-    if (QCLOUD_RET_SUCCESS == IOT_DynReg_Device(&sDevInfo) && nRtn > 0)
+    if (!strcmp(argv[1], "get_device_secret"))
     {
-        Log_d("dynamic register success,productID: %s, devName: %s, device_secret: %s", sDevInfo.product_id, sDevInfo.device_name, sDevInfo.device_secret);
-    }
-    else
-    {
-        Log_e("%s dynamic register fail", sDevInfo.device_name);
+        code = get_device_secret(&sDevInfo);
+        if (code == QCLOUD_RET_SUCCESS)
+        {
+            printf("[success]%s", sDevInfo.device_secret);
+        }
+        else
+        {
+            printf("[error] %d", code);
+        }
     }
 
     return 0;
