7e56350e6534844dc0baa29b0cf6b47ba2bd8b09
wailovet <19940108>
2019-11-26 11:36:44 +0800

    `update`

--- samples/ouxuanac/ouxuanac_sample.c
+++ samples/ouxuanac/ouxuanac_sample.c
@@ -7,6 +7,32 @@
 #include "qcloud_iot_export.h"
 #include "qcloud_iot_import.h"
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <net/if.h>        //for struct ifreq
+
+int get_mac(char * mac, int len_limit)    //返回值是实际写入char * mac的字符个数（不包括'\0'）
+{
+    struct ifreq ifreq;
+    int sock;
+
+    if ((sock = socket (AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror ("socket");
+        return -1;
+    }
+    strcpy (ifreq.ifr_name, "eth0");    //Currently, only get eth0
+
+    if (ioctl (sock, SIOCGIFHWADDR, &ifreq) < 0)
+    {
+        perror ("ioctl");
+        return -1;
+    }
+    
+    return snprintf (mac, len_limit, "%X:%X:%X:%X:%X:%X", (unsigned char) ifreq.ifr_hwaddr.sa_data[0], (unsigned char) ifreq.ifr_hwaddr.sa_data[1], (unsigned char) ifreq.ifr_hwaddr.sa_data[2], (unsigned char) ifreq.ifr_hwaddr.sa_data[3], (unsigned char) ifreq.ifr_hwaddr.sa_data[4], (unsigned char) ifreq.ifr_hwaddr.sa_data[5]);
+}
+
 int main()
 {
     //init log level
